990dd179ecb0eb913c7787cc554030d597369ad0
Obit01337 <74430714+Obit01337@users.noreply.github.com>
2020-11-23 15:04:04 +0300

    1.1

--- main.c
+++ main.c
@@ -1,16 +1,24 @@
+//TASK:
+//Enter text in the head function.
+//In the first function, calculate the number of words in the text.
+//In the second, count the number of words for which the first and last characters are the same.
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int check();
-int count_num_words(char** text, int num_str, int length);
+int count_num_words(char** text, int num_str);
+int count_same_first_last_symbols(char** text, int num_str);
 
 int main()
 {
+    // Input text size
     puts("Enter number of strings");
     int num_str = check();
     puts("Enter max length of string");
     int length = check();
     char **text = NULL;
+    // 22-31 creating array for text
     if (!(text = (char**)malloc(num_str * sizeof(char*))))
         return 144;
     for (int i = 0; i < num_str; ++i)
@@ -22,15 +30,23 @@ int main()
             return 145;
         }
     puts("Enter your text");
+    // Input text
     for (int i = 0; i < num_str; ++i)
     {
         fgets(text[i], length+1, stdin);
         rewind(stdin);
     }
+    // Output text
     for (int i = 0; i < num_str; ++i)
         printf_s("\n%d %s", i, text[i]);
-    int num = count_num_words(text, num_str, length);
-    printf_s("\n%d words in the text", num);
+    // Outputs results of functions
+    printf_s("\n%d words in the text", count_num_words(text, num_str));
+    printf_s("\n%d words with the same first and last symbols in the text", count_same_first_last_symbols(text, num_str));
+    // Freeing memory
+    for (int i = 0; i < num_str; ++i)
+        free(text[i]);
+    free(text);
+    getchar();
     return 0;
 }
 
@@ -47,11 +63,12 @@ int check()
     return number;
 }
 
-int count_num_words(char** text, int num_str, int length)
+// The function counts the number of words in the text
+int count_num_words(char** text, int num_str)
 {
     int num = 0;
     for (int i = 0; i < num_str; ++i)
-        for (int j = 0; j < length && text[i][j] != '\0' && text[i][j] != '\n'; ++j)
+        for (int j = 0; text[i][j] != '\0' && text[i][j] != '\n'; ++j)
             if ((j == 0 && text[i][j] != ' ' && text[i][j] != '\t') || (text[i][j] != ' ' && text[i][j] != '\t' && (text[i][j-1] == ' ' || text[i][j-1] == '\t')))
             {
                 num++;
@@ -60,3 +77,22 @@ int count_num_words(char** text, int num_str, int length)
             }
     return num;
 }
+
+// The function counts the number of words with the same beginning and end
+int count_same_first_last_symbols(char** text, int num_str)
+{
+    int num = 0;
+    for (int i = 0; i < num_str; ++i)
+        for (int j = 0; text[i][j] != '\0' && text[i][j] != '\n'; ++j)
+            if ((j == 0 && text[i][j] != ' ' && text[i][j] != '\t') || (text[i][j] != ' ' && text[i][j] != '\t' && (text[i][j-1] == ' ' || text[i][j-1] == '\t')))
+            {
+                char first = text[i][j];
+                for (int k = j; text[i][k] != ' ' && text[i][k] != '\n' && text[i][k] != '\t' && text[i][k] != '\0'; ++k)
+                    j = k;
+                char last = text[i][j];
+                if (first == last)
+                    num++;
+            }
+    return num;
+}
+
