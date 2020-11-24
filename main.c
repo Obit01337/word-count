082081c5ae66c39745ad86d39f45822d75a92de4
Obit01337 <74430714+Obit01337@users.noreply.github.com>
2020-11-24 22:01:57 +0300

    1.3

--- main.c
+++ main.c
@@ -6,62 +6,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int check();
+int check_correct_input(void);
 int count_num_words(char** text, int num_str);
 int count_same_first_last_symbols(char** text, int num_str);
 
-int main()
+int main(void)
 {
     // Input text size
     puts("Enter number of strings");
-    int num_str = check();
+    int num_str = check_correct_input();
     puts("Enter max length of string");
-    int length = check();
+    int size_str = check_correct_input();
     char **text = NULL;
-    // 22-31 creating array for text
+    // Creating array for text
     if (!(text = (char**)malloc(num_str * sizeof(char*))))
+    {
+        puts("Not enough memory");
+        system("pause");
         return 144;
+    }
     for (int i = 0; i < num_str; ++i)
-        if (!(text[i] = (char *) malloc((length + 1) * sizeof(char))))
+        if (!(text[i] = (char *)malloc((size_str + 1) * sizeof(char))))
         {
             for (int j = 0; j < i; ++j)
                 free(text[j]);
             free(text);
+            puts("Not enough memory");
+            system("pause");
             return 145;
         }
     puts("Enter your text");
     // Input text
     for (int i = 0; i < num_str; ++i)
     {
-        fgets(text[i], length+1, stdin);
+        fgets(text[i], size_str + 1, stdin);
         rewind(stdin);
     }
     // Output text
     for (int i = 0; i < num_str; ++i)
-        printf_s("\n%d %s", i, text[i]);
+        printf_s("%d %s\n", i, text[i]);
     // Outputs results of functions
-    printf_s("\n%d words in the text", count_num_words(text, num_str));
-    printf_s("\n%d words with the same first and last symbols in the text", count_same_first_last_symbols(text, num_str));
+    printf_s("%d words in the text\n", count_num_words(text, num_str));
+    printf_s("%d words with the same first and last symbols in the text\n", count_same_first_last_symbols(text, num_str));
     // Freeing memory
     for (int i = 0; i < num_str; ++i)
         free(text[i]);
     free(text);
-    printf_s("\n");
-    getchar();
+    system("pause");
     return 0;
 }
 
 // Function for correct input
-int check()
+int check_correct_input(void)
 {
-    int number;
-    char check;
+    int num;
+    char enter;
     do
     {
         rewind(stdin);
-        puts("Input\t");
-    }while ( !(scanf_s("%d%c", &number, &check)) || check != '\n' || number <= 0);
-    return number;
+        puts("Input");
+    }while (!(scanf_s("%d%c", &num, &enter)) || enter != '\n' || num <= 0);
+    return num;
 }
 
 // The function counts the number of words in the text
